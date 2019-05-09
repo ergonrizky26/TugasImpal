e4878fa892749c79249365c1131620f5ba0af52e
ergonrizky26 <38247106+ergonrizky26@users.noreply.github.com>
2019-05-09 22:21:47 +0700

    Add files via upload

---
+++ Apotek_Impal_Update/view/index.php
@@ -0,0 +1,14 @@
+<?php
+  error_reporting(0);
+?>
+<html>
+<head>
+<title>Sistem Informasi Apotek</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="mos-css/mos-style.css"> <!--pemanggilan file css-->
+</head>
+
+<body>
+<?php include "kiri.php"; ?>
+</body>
+</html>
